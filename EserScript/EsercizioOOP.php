12709a805921bd52f6c761cfc7c87591537ab5a7
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-11-30 18:40:21 +0100

    commit

--- EserScript/EsercizioOOP.php
+++ EserScript/EsercizioOOP.php
@@ -1,68 +1,63 @@
 <?php
 class Calcolatrice{
     public $Operazioni;
+    // public $sum = array();
+    // public $diff = array();
 
     function __construct($Operazioni) {
         $this->Operazioni = array(); 
     }
     function sum($x,$y){
-        $sum++;
         $result = $x + $y;
         $op = new Operazione('somma',$x,$y,$result);
         array_push($this->Operazioni,$op);
+        // array_push($this->sum,'+1');
         return $result;
     }
     function difference($x,$y){
-        $diff++;
         $result = $x - $y;
         $op = new Operazione('sottrazione',$x,$y,$result);
         array_push($this->Operazioni,$op);
+        // array_push($this->diff,'+1');
         return $result;
     }
     function multiplication($x,$y){
-        $per++;
         $result = $x * $y;
         $op = new Operazione('moltiplicazione',$x,$y,$result);
         array_push($this->Operazioni,$op);
         return $result;
     }
     function division($x,$y){
-        $div++;
         $result = $x / $y;
         $op = new Operazione('divisione',$x,$y,$result);
         array_push($this->Operazioni,$op);
         return $result;
     }
     function exponentiation($x,$y){
-        $esp++;
         $result = pow($x,$y);
-        $opEspo = new Operazione('esponente',$x,0,$result);
-        array_push($this->Operazioni,$y);
+        $opEspo = new Operazione('esponente',$x,'exp:'.$y,$result);
+        array_push($this->Operazioni,$opEspo);
         return $result;
     }
     function sqrtFun($x){
-        $rq++;
         $result = sqrt($x);
         $op = new Operazione('radicequadrata',$x,0,$result);
         array_push($this->Operazioni,$op);
         return $result;
     }
     function converter($x){
-        $conv1++;
         $result = decbin($x);
         $op = new Operazione('Dec-Bin',$x,0,$result);
         array_push($this->Operazioni,$op);
         return $result;
     }
     function UNconverter($x){
-        $conv2++;
         $result = bindec($x);
         $op = new Operazione('Bin-Dec',$x,0,$result);
         array_push($this->Operazioni,$op);
         return $result;
     }
 }
-
 
 class Operazione {
     public $Opname;
@@ -79,16 +74,41 @@ class Operazione {
 }
 
 $CalcolatriceProva = new Calcolatrice($Operazioni);
-echo "\n";
-echo $CalcolatriceProva->sum(1,1);echo "\n";
-echo $CalcolatriceProva->difference(4,1);echo "\n";
-echo $CalcolatriceProva->multiplication(2,10);echo "\n";
-echo $CalcolatriceProva->division(10,2);echo "\n";
-echo $CalcolatriceProva->exponentiation(2,5);echo "\n";
-echo $CalcolatriceProva->sqrtFun(25);echo "\n";
-echo $CalcolatriceProva->converter(1);echo "\n";
-echo $CalcolatriceProva->UNconverter(001);echo "\n";
-echo "\n";
-print_r($CalcolatriceProva->Operazioni)
+$CalcolatriceProva->sum(1,1);
+$CalcolatriceProva->difference(4,1);
+$CalcolatriceProva->multiplication(2,10);
+$CalcolatriceProva->division(10,2);
+$CalcolatriceProva->exponentiation(2,5);
+$CalcolatriceProva->sqrtFun(25);
+$CalcolatriceProva->converter(1);
+$CalcolatriceProva->UNconverter(001);
+print_r($CalcolatriceProva->Operazioni);
+
+
+// $sum = 0;
+// $diff = 0;
+// $molt = 0;
+// for ($i=0; $i < count($CalcolatriceProva->Operazioni) ; $i++) { 
+//     if ($CalcolatriceProva->Operazioni[$i]->Opname === "somma"){
+//         $sum++;
+//     }
+//     if ($CalcolatriceProva->Operazioni[$i]->Opname === "sottrazione"){
+//         $diff++;
+//     }
+//     if ($CalcolatriceProva->Operazioni[$i]->Opname === "moltiplicazione"){
+//         $molt++;
+//     }
+//     if($sum > $diff && $sum > $molt){
+//        $res = 'ci sono più somme';
+//     }
+//     if($diff > $sum && $diff > $molt){
+//         $res = 'ci sono più diff';
+//     }
+// } 
+// echo $res . "\n";
+
+
+
+
 
 ?>
